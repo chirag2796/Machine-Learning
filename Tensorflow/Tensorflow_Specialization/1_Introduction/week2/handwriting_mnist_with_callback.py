9c882f2ec8a51f7f54aa652ef0bcbb432467156f
unknown <chirag2796@gmail.com>
2019-09-05 20:03:21 +0530

    week 2

---
+++ Tensorflow/Tensorflow_Specialization/1_Introduction/week2/handwriting_mnist_with_callback.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('acc')*100 > 99.0:
+            print("\n Reached 99% accuracy so stopping training")
+            self.model.stop_training = True
+
+def train_mnist():
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train/255.0
+    x_test = x_test/255.0
+
+    model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+                                        tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=10, callbacks=[myCallback()])
+    return history.epoch, history.history['acc'][-1]
+
+q, w = train_mnist()
+print(q)
+print(w)
